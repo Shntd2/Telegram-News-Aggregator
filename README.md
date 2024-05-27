d01a03bc80a3ec4e6e56f4fe12668138a173f56f
shntd2 <pmpm68036@gmail.com>
2024-05-27 23:39:37 +0300

    Updated README.MD and bot_description.py

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 This is Postman, a news aggregator bot
 
-It sends the top 20 worldwide news in English twice a day at 08:00 and 20:00
+It sends the top 20 worldwide news in English twice a day at 08:00 AM and 08:00 PM
 
-Postman sources include Google News and newsAPI top headlines
+Postman sources include Google News and News API top headlines
 
 To use it:
 1. Push /start command
 2. Select preferred time zone from provided options
+   2.1 All available time zones are located in `specific_timezones.py`
 3. Subscription is activated
 
 Bots available commands:
--- app_package/bot_description.py
+++ app_package/bot_description.py
@@ -1,6 +1,6 @@
 description = ('This is Postman, a news aggregator bot'
-               '\n\nIt sends the top 20 worldwide news in English twice a day at 08:00 and 20:00'
-               '\n\nPostman sources include Google News and newsAPI top headlines'
-               '\n\nTo use it, follow these steps:'
+               '\n\nIt sends the top 20 worldwide news in English twice a day at 08:00 AM and 08:00 PM'
+               '\n\nPostman sources include Google News and News API top headlines'
+               '\n\nTo use it:'
                '\n1. Push /start command\n2. Select preferred time zone from provided options'
                '\n3. Subscription is activated')
